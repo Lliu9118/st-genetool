d8b5eaff053069b99a527ad24350161c6c35f387
Lliu9118 <115491253+Lliu9118@users.noreply.github.com>
2023-02-11 16:29:20 -0500

    Update site.py

--- site.py
+++ site.py
@@ -2,6 +2,7 @@ import requests
 import streamlit as st
 import array as ary
 import pandas as pd
+import altair as alt
 
 
 st.set_page_config(page_title="My Webpage", page_icon=":skull:", layout="wide")
@@ -11,9 +12,14 @@ a = ary.array('i',[5])
 
 
 
-chart_data={"gene":["gene1","gene2","gene3"],"mutationrate":[0.009,0.003,0.006]
-chart_data = pd.DataFrame(chart_data)
-chart_data=chart_data.set_index("name")
+source = pd.DataFrame({
+    'Gene': ['gene1','gene2','gene3'],
+    'Mutationrate': [0.009,0.003,0.006]
+})
+bar_chart=alt.Chart(source).mark_bar().encode(
+    x='Gene',
+    y='Mutationrate',
+)
 
 
 
@@ -61,7 +67,7 @@ with st.container():
         (st.write('Link: Asian50.org'))
  
     if option == 'BRCA' and option2 == 'Asian' and color == '49':
-        st.bar_chart(chart_data)
+        st.altair_chart(bar_chart,use_container_width=True)
         
 
 
