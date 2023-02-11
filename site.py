879f7f88ab1c7740886c71d2c96e97ea48fea019
Lliu9118 <115491253+Lliu9118@users.noreply.github.com>
2023-02-10 21:33:31 -0500

    Update site.py

--- site.py
+++ site.py
@@ -1,16 +1,10 @@
 import requests
 import streamlit as st
-from streamlit_lottie import st_lottie
 
 st.set_page_config(page_title="My Webpage", page_icon=":skull:", layout="wide")
 
-def load_lottieurl(url):
-    r = requests.get(url)
-    if r.status_code != 200:
-        return None
-    return r.json()
 
-lottie_coding = load_lottieurl("https://assets9.lottiefiles.com/packages/lf20_8olbnwvj.json")
+
 
 with st.container():
     st.title("The Gene Mutation Interactive Tool")
